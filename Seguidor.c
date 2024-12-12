79073524081b29d3878ed2827bd836de149cf0a4
Artsm12 <16869841624@estudante.ufjf.br>
2024-12-12 13:14:04 -0300

    corrige erros

--- Seguidor.c
+++ Seguidor.c
@@ -3,27 +3,27 @@
 #pragma config(Motor,  motorB,          MotorDireito,  tmotorNXT, PIDControl, driveRight, encoder)
 
 //Velocidade base do robo
-#define velocidadeBase 50
+#define VelocidadeBase 50
 
 //Quantidade de luz captada pelo sensor (possivelmente vai ser alterado)
-#define ReflexoLuz = 30;	
+#define ReflexoLuz 30
 
 task main(){
 
 	while(true){
-		
+
 		//Quando o sensor ver a parte preta da percurso, ele vai fazer curva para a direita
-		if(sensorValue[SensorDeLuz] < ReflexoLuz){
+		if(SensorValue[SensorDeLuz] < ReflexoLuz){
 			motor[MotorEsquerdo] = VelocidadeBase;
 			motor[MotorDireito] = 0;
 		}
-		
+
 		//Quando o sensor ver a parte branca do percurso, ele vai fazer curva para a esquerda
 		else{
 			motor[MotorEsquerdo] = 0;
 			motor[MotorDireito] = VelocidadeBase;
 		}
-		
+
 	//Tempo de espera para nao sobrecarregar o robo
 	wait1Msec(1);
 	}
